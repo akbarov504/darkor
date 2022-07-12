cc977b0eb04e3d1f0fa79bff56bfdb429d607c5b
nodir <nodirbekjuraev02@gmail.com>
2022-07-12 15:54:02 +0500

    mapper fixed

--- src/main/java/uz/darkor/darkor_22/mapper/GenericMapper.java
+++ src/main/java/uz/darkor/darkor_22/mapper/GenericMapper.java
@@ -7,19 +7,19 @@ import uz.darkor.darkor_22.entity.BaseEntity;
 import java.util.List;
 
 public interface GenericMapper<CD extends BaseDTO, UD extends GenericDTO, GD extends GenericDTO, E extends BaseEntity> extends BaseMapper {
-    E toCreateDTO(CD DTO);
+    CD toCreateDTO(E DTO);
 
-    CD fromCreateDTO(E entity);
+    E fromCreateDTO(CD entity);
 
-    E toUpdateDTO(UD DTO);
+    UD toUpdateDTO(E DTO);
 
-    UD fromUpdateDTO(E entity);
+    E fromUpdateDTO(UD entity);
 
-    E toGetDTO(GD DTO);
+    GD toGetDTO(E DTO);
 
-    GD fromGetDTO(E entity);
+    E fromGetDTO(GD entity);
 
-    List<E> toListDTO(List<GD> DTO);
+    List<GD> toListDTO(List<E> DTO);
 
-    List<GD> fromListDTO(List<E> entity);
+    List<E> fromListDTO(List<GD> entity);
 }
