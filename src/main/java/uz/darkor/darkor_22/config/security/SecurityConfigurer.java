8a15b4ff2fee3498d4185582ee4ec64ac86f514e
nodir <nodirbekjuraev02@gmail.com>
2022-07-21 16:55:11 +0500

    a

--- src/main/java/uz/darkor/darkor_22/config/security/SecurityConfigurer.java
+++ src/main/java/uz/darkor/darkor_22/config/security/SecurityConfigurer.java
@@ -2,11 +2,21 @@ package uz.darkor.darkor_22.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import uz.darkor.darkor_22.config.security.filter.CustomAuthenticationFilter;
+import uz.darkor.darkor_22.config.security.filter.CustomAuthorizationFilter;
+import uz.darkor.darkor_22.utils.BaseUtils;
 
 @Configuration
 @EnableGlobalMethodSecurity(
@@ -14,33 +24,34 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
         securedEnabled = true,
         jsr250Enabled = true)
 public class SecurityConfigurer extends GlobalMethodSecurityConfiguration {
+
     public static final String[] WHITE_LIST = {
-            "/auth/login", "/auth/register"
+            BaseUtils.PATH + "/auth/login",
+            BaseUtils.PATH + "/auth/register",
+            BaseUtils.PATH + "/token",
+            "/swagger-ui/**",
+            "/api-docs/**",
+            BaseUtils.PATH + "/auth/accept/**"
     };
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(expressionInterceptUrlRegistry ->
-                        expressionInterceptUrlRegistry
-                                .antMatchers(WHITE_LIST).permitAll()
-                                .anyRequest()
-                                .authenticated()
-                )
-                .formLogin(httpSecurityFormLoginConfigurer ->
-                        httpSecurityFormLoginConfigurer
-                                .loginPage("/auth/login")
-                                .defaultSuccessUrl("/auth/home", true)
-                                .loginProcessingUrl("/auth/login")
-                )
-                .logout(httpSecurityLogoutConfigurer ->
-                        httpSecurityLogoutConfigurer
-                                .logoutUrl("/auth/logout")
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))
-                                .invalidateHttpSession(true)
-                                .clearAuthentication(true)
-                );
+                .authorizeRequests()
+                .antMatchers(WHITE_LIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling().authenticationEntryPoint(userAuthenticationEntryPoint)
+                .csrf().disable()
+                .cors().disable();
+
         return http.build();
+
+
     }
 
 
